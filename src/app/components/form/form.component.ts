41a2d1e6c1316eb95f8b2b40c6abd7cb1cc128c9
noel <marianoelmorando@gmail.com>
2023-08-04 18:42:46 +0200

    creacion del formulario y validadores

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./form.component.css']
 })
 export class FormComponent {
+  userForm:FormGroup
+  constructor(){
+    this.userForm = new FormGroup({
+      userName: new FormControl("",[]),
+      userSurname: new FormControl("",[]),
+      userEmail: new FormControl("",[]),
+      userImg: new FormControl("",[]),
+    })
+  }
 
+getDataForm():void{
+  console.log(this.userForm.value)
+}
 }
